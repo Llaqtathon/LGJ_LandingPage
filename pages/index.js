5274be2ffed9d82467ad14c074dd612e0f5ef97a
Maria Isabel Lopez <mrlpz1515@gmail.com>
2022-04-28 14:37:31 -0500

    Main test

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Lima Game Jam Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
